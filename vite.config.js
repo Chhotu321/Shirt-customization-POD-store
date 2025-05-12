cd174fa6d4c50891a9d41f15ed2174647cd910f7
Ketanop321 <ketanop473@gmail.com>
2025-05-12 23:20:45 +0530

    fixing viteconfigjs file

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    minify: "terser",
+    // Change from 'terser' to 'esbuild' (which is included in Vite)
+    minify: "esbuild",
     sourcemap: true,
   },
 })
